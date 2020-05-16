ee04144ae6b9dc3c3769d17d42878d4d7b88dfcc
Alouie412 <alouie412@gmail.com>
2020-05-16 21:11:28 +0000

    Initial commit: API status checker

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,26 @@
+$( document ).ready(function () {
+  const amenityList = {};
+  $('input[type="checkbox"]').click(function () {
+    $(this).each(function () {
+      if (this.checked === true) {
+        amenityList[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenityList[$(this).data('id')];
+      }
+    });
+
+    if (Object.values(amenityList).length > 0) {
+      $('.amenities h4').text(Object.values(amenityList).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
+  });
+});
+
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+  if (data.status === 'OK') {
+    $('DIV#api_status').addClass('available');
+  } else {
+    $('DIV#api_status').removeClass('available');
+  }
+});
