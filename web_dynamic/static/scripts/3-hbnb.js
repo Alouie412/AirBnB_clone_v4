9b743487d13fab772340a7c69cd15908ce6a86d1
Ben Keener <keener4christ@gmail.com>
2020-05-17 20:50:52 +0000

    Added proper price styling

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -38,8 +38,8 @@ $.ajax({
         '<article>' +
           '<div class="title_box">' +
             `<h2>${place.name}</h2>` +
+            `<div class="price_by_night">$${place.price_by_night}</div>` +
           '</div>' +
-          `<div class="price_by_night">${place.price_by_night}</div>` +
           '<div class="information">' +
             `<div class="max_guest">${place.max_guest} Guest${place.max_guest > 1 ? 's' : ''}</div>` +
             `<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms > 1 ? 's' : ''}</div>` +
