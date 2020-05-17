e480d09a805f10e985df69cbddfdf61d5ebf8509
Ben Keener <keener4christ@gmail.com>
2020-05-17 21:04:39 +0000

    Fixed templating. Changed jquery to wait for load

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -15,45 +15,69 @@ $(document).ready(function () {
       $('.amenities h4').html('&nbsp');
     }
   });
-});
-
-$.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
-  if (data.status === 'OK') {
-    $('DIV#api_status').addClass('available');
-  } else {
-    $('DIV#api_status').removeClass('available');
-  }
-});
-
-$.ajax({
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  contentType: 'application/json',
-  dataType: 'json',
-  data: '{}',
-  success: function (data) {
-    let placeList;
-    for (placeList of Object.values(data)) {
-      // Find a better way to write this nightmare of a line. Maybe use Mustache.js for template
-      $('section.places').append('<article><div class="title_box"><h2>' + placeList.name + '</h2></div>' + '<div class="price_by_night">' + placeList.price_by_night + '</div>' + '<div class="information">' + '<div class="max_guest">' + placeList.max_guest + ' Guest</div>' + '<div class="number_rooms">' + placeList.number_rooms + ' Bedroom</div>' + '<div class="number_bathrooms">' + placeList.number_bathrooms + ' Bathroom</div>' + '</div>' + '<div class="description">' + placeList.description + '</div></article>');
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data, status) {
+    if (data.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
     }
-  }
-});
+  });
 
-$('button').click(function () {
   $.ajax({
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search/',
     contentType: 'application/json',
     dataType: 'json',
-    data: JSON.stringify({ amenities: Object.keys(amenityList) }),
+    data: '{}',
     success: function (data) {
-      $('article').remove();
-      let placeList;
-      for (placeList of Object.values(data)) {
+      for (const place of Object.values(data)) {
         // Find a better way to write this nightmare of a line. Maybe use Mustache.js for template
-        $('section.places').append('<article><div class="title_box"><h2>' + placeList.name + '</h2></div>' + '<div class="price_by_night">' + placeList.price_by_night + '</div>' + '<div class="information">' + '<div class="max_guest">' + placeList.max_guest + ' Guest</div>' + '<div class="number_rooms">' + placeList.number_rooms + ' Bedroom</div>' + '<div class="number_bathrooms">' + placeList.number_bathrooms + ' Bathroom</div>' + '</div>' + '<div class="description">' + placeList.description + '</div></article>');
+        $('section.places').append(
+          '<article>' +
+            '<div class="title_box">' +
+              `<h2>${place.name}</h2>` +
+              `<div class="price_by_night">$${place.price_by_night}</div>` +
+            '</div>' +
+            '<div class="information">' +
+              `<div class="max_guest">${place.max_guest} Guest${place.max_guest > 1 ? 's' : ''}</div>` +
+              `<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms > 1 ? 's' : ''}</div>` +
+              `<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms > 1 ? 's' : ''}</div>` +
+            '</div>' +
+            `<div class="description">${place.description}</div>` +
+          '</article>');
       }
     }
   });
+
+  $('button').click(function () {
+    console.log("clicked");
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ amenities: Object.keys(amenityList) }),
+      success: function (data) {
+        console.log("Got data");
+        $('article').remove();
+        for (const place of Object.values(data)) {
+          // Find a better way to write this nightmare of a line. Maybe use Mustache.js for template
+          $('section.places').append(
+            '<article>' +
+              '<div class="title_box">' +
+                `<h2>${place.name}</h2>` +
+                `<div class="price_by_night">$${place.price_by_night}</div>` +
+              '</div>' +
+              '<div class="information">' +
+                `<div class="max_guest">${place.max_guest} Guest${place.max_guest > 1 ? 's' : ''}</div>` +
+                `<div class="number_rooms">${place.number_rooms} Bedroom${place.number_rooms > 1 ? 's' : ''}</div>` +
+                `<div class="number_bathrooms">${place.number_bathrooms} Bathroom${place.number_bathrooms > 1 ? 's' : ''}</div>` +
+              '</div>' +
+              `<div class="description">${place.description}</div>` +
+            '</article>');
+        }
+      }
+    });
+  });
 });
+
